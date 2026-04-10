97b4584379a9990b2ff14408d7c084e4f088fce1
josemartinthomas14-ui <josemartinthomas14@gmail.com>
2026-04-10 13:40:09 +0530

    Update ChessboardSum.java

--- src/ChessboardSum.java
+++ src/ChessboardSum.java
@@ -1,3 +1,31 @@
+import java.util.Scanner;
+
+public class ChessboardSum {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            if (!scanner.hasNextInt()) return;
+            int n = scanner.nextInt();
+            long blackSum = 0;
+            long whiteSum = 0;
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (scanner.hasNextInt()) {
+                        int value = scanner.nextInt();
+                        if ((i + j) % 2 == 0) {
+                            blackSum += value;
+                        } else {
+                            whiteSum += value;
+                        }
+                    }
+                }
+            }
+
+            System.out.println(blackSum);
+            System.out.println(whiteSum);
+        }
+    }
+}
 public class ChessboardSum {
     
         // TODO: Read N
