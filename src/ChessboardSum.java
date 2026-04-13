bfa8be887a25d239e528da58c3838fabe791d123
josemartinthomas14-ui <josemartinthomas14@gmail.com>
2026-04-13 10:36:00 +0530

    Update ChessboardSum.java

--- src/ChessboardSum.java
+++ src/ChessboardSum.java
@@ -26,12 +26,3 @@ public class ChessboardSum {
         }
     }
 }
-public class ChessboardSum {
-    
-        // TODO: Read N
-        // TODO: Read the matrix
-        // TODO: Iterate through the matrix
-        // TODO: Check if (i + j) is even (Black) or odd (White)
-        // TODO: Accumulate sums and print them
-    
-}
